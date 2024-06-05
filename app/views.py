7db03e1d2cc26941a4d35c2f13c650c31b19e54b
pokalapriyanka <pokalapriyanka123@gmail.com>
2024-06-05 23:59:33 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,9 +3,9 @@ from app.forms import *
 from django.http import HttpResponse,HttpResponseRedirect
 from django.core.mail import send_mail
 from django.urls import reverse
-
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def home(request):
     if request.session.get('username'):
@@ -26,12 +26,12 @@ def registration(request):
             pw=NMUFDO.cleaned_data['password']
             MUFDO.set_password(pw)
             MUFDO.save()
-            send_mail('Registration',
+            '''send_mail('Registration',
             'Thank you for Registering',
             'p9584520@gmail.com',
             [MUFDO.email],
             fail_silently=False
-            )
+            )'''
             MPFDO=NMPFDO.save(commit=False)
             MPFDO.username=MUFDO
             MPFDO.save()
@@ -45,7 +45,6 @@ def user_login(request):
         if request.method=='POST':
             username=request.POST['un']
             password=request.POST['pw']
-
             AUO=authenticate(username=username,password=password)
             if AUO and AUO.is_active:
                 login(request,AUO)
@@ -60,3 +59,23 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('home'))
+
+@login_required
+def profile_display(request):
+    username=request.session.get('username')
+    UO=User.objects.get(username=username)
+    Po=Profile.objects.get(username=UO)
+    d={'UO':UO,'PO':Po}
+    return render(request,'profile_display.html',d)
+
+
+def change_password(request):
+    if request.method=='POST':
+        username=request.session.get('username')
+        UO=User.objects.get(username=username)
+        nw=request.POST['nw']
+        UO.set_password(nw)
+        UO.save()
+        return HttpResponse('Password Changed Successfully')
+
+    return render(request,'change_password.html')
